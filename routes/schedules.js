38b631f4dbdeb4e59be625c9fd541411261e33c6
Cheahuychou Mao <cmao18@mit.edu>
2017-01-17 23:57:28 +0400

    Resolve merge conflict

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
-
-
+var PythonShell = require('python-shell');
 var Schedule = require("../models/schedule.js");
 var Registration = require("../models/registration.js");
 
@@ -15,24 +14,34 @@ var Registration = require("../models/registration.js");
 // gets all the registration objects and feed those to the python script 
 // to get the pairs
 router.get('/match', function(req, res, next) {
-	Registration.getAllUnmatchedRegistrations(err, registrations) {
+
+	Registration.getUnmatchedRegistrations(function (err, registrations) {
+		var registrations = [{'user': '1111', 'availability': {'0': ['11:00 - 13:00'], '3': ['2:00 - 5:00']},
+							'genderPref': ['Male', 'Female'], 'course': 'Intermediate English',
+							'isMatched': false},
+						  {'user': '1112', 'availability': {'1': ['10:00 - 12:00'], '5': ['1:00 - 5:00']},
+							'genderPref': ['Female'], 'course': 'Intermediate English',
+							'isMatched': false}
+						];
+
+		var city_capacity = {'Boston': 10, 'Cambridge': 5, 'Bangkok': 3};
+
 		var options = {
 			mode: 'json',
 			scriptPath: './scheduler/',
-			args: [JSON.stringify(registrations)]
+			args: [JSON.stringify(registrations), JSON.stringify(city_capacity)]
 		}
 
-		PythonShell.run('match.py', options, function (err, results) {
+		PythonShell.run('match.py', options, function (err, matches) {
 		  if (err) {
 		  	throw err;
 		  }
-		  // results is an array consisting of messages collected during execution
-		  console.log('results:', typeof results, results);
+		  // matches is an array consisting of messages collected during execution
+		  console.log('matches:', typeof matches, matches);
 		});
 
 		// parse the string and write to db
-	}
-	}
+	});
 });
 
 module.exports = router; //keep at the bottom of the file
