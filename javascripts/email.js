928f1e80ddfcd3fbeeb5ca887dca7991d64422ab
Karleigh Moore <karleighmoore19@gmail.com>
2017-01-22 22:14:07 -0500

    Email works again

--- javascripts/email.js
+++ javascripts/email.js
@@ -96,7 +96,7 @@ var Email = function() {
             var content = '{}<p>Hi {}!<br><br>Confirm your Pax Populi Scheduler account by clicking on the confirm button below.<form action="{}"><input type="submit" value="Confirm" /></form>{}</p>'.format(that.welcomeMessage, user.firstName, link, that.signature);
             console.log('about to send a verification email to', user.email);
             sendEmail(user.email, subject, content, callback);
-            callback(null, user);
+            //callback(null, user); //breaks the app
         });
     };
 
@@ -137,7 +137,7 @@ var Email = function() {
             console.log(content);
             console.log('about to send an approval request email to', user.email);
             sendEmail(config.adminEmailAddress(), subject, content, callback); // for now send it back to the user
-            callback(null, user);
+            //callback(null, user); //breaks app
         });
     };
 
