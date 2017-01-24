a0566e73222d180fd1a92ee951728b5e4821a0f0
Karleigh Moore <karleighmoore19@gmail.com>
2017-01-24 11:56:21 -0500

    Updated schemas

--- models/schedule.js
+++ models/schedule.js
@@ -5,21 +5,24 @@ var User = require("../models/user.js");
 var utils = require("../javascripts/utils.js");
 
 
-//TODO need validators
-
-//TODO check isTutor for tutor/student objects
-
 var scheduleSchema = mongoose.Schema({
     student: {type: ObjectId, ref:"User", required:true},
     tutor: {type: ObjectId, ref:"User", required:true},
-    studentClassSchedule: {type: [String], required:true},
-    tutorClassSchedule: {type: [String], required:true},
-    UTCClassSchedule: {type: [String], required:true},
+    possibleCourses: {type:[String], required:true}, 
+    studentCoord :{type: ObjectId, ref:"User"},
+    tutorCoord :{type: ObjectId, ref:"User"},
+    studentClassSchedule: {type: [Date], required:true},
+    tutorClassSchedule: {type: [Date], required:true},
+    UTCClassSchedule: {type: [Date], required:true}, // for Admins
     adminApproved: {type: Boolean, required: true, default: false},
     tutorApproved: {type: Boolean, required: true, default: false},
     studentApproved: {type: Boolean, required: true, default: false},
-    firstDay: {type: String, required:true}, 
-    lastDay: {type: String, required: true},
+    firstDateTime: {type: Date, required:true}, 
+    lastDateTime: {type: Date, required: true}, //so we know when to delete the schedule from the DB
+    studentPossibleSchedules: {type:mongoose.Schema.Types.Mixed},
+    tutorPossibleSchedules: {type:mongoose.Schema.Types.Mixed},
+    UTCPossibleSchedules: {type:mongoose.Schema.Types.Mixed},
+    course: {type: String} //we don't know yet how ppl will agree on a course
 });
 
 
@@ -39,6 +42,7 @@ scheduleSchema.statics.getSchedules = function (user, callback) {
             if (err) {
                 callback({success: false, message: err.message});
             } else {
+                //these need to be updated the the country/region/school that the coordinator is in charge of
                 Schedule.find( {$or: [{country: user.country}, {region: user.region}, {school: user.school}]}, function (err, schedules) {
                     if (err) {
                         callback({success: false, message: err.message});
@@ -49,10 +53,8 @@ scheduleSchema.statics.getSchedules = function (user, callback) {
             }
         })
     }
-    
 }
 
-//how to run processes in the background like once a day at some time
 
 
 //keep at bottom of file
