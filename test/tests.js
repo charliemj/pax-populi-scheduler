ff319b365d49d47c058fc13a331d30bcca020cc8
Karleigh Moore <karleighmoore19@gmail.com>
2017-01-27 21:39:38 -0500

    Added tests

--- test/tests.js
+++ test/tests.js
@@ -8,8 +8,10 @@ var db = mongoose.connect('mongodb://localhost/testdb');
 
 
 var availTest = { '0':[[ '23:00', '24:00' ] ], '1': [],'2': [],'3': [],'4': [],'5': [],'6': [[ '06:00', '08:30' ]] }; 
-var TEST_USER = { "_id" : "588b9d45b3aada3ec847430c", "username" : "kjmoore21", "password" : "$2a$10$rNXfY2VdUZ2eCRVVOwjS2.4uagb1rbTaQRUMrQH3/uO5CIs9KDv9q", "role" : "Tutor", "email" : "karleighmoore19+osiusj@gmail.com", "firstName" : "Karleigh", "lastName" : "Moore", "phoneNumber" : "6173888354", "gender" : "Male", "interests" : [ "Business", "International Relations" ], "nationality" : "United States", "timezone" : "America/Whitehorse", "region" : "Guam", "country" : "United States", "major" : "Management", "enrolled" : "false", "educationLevel" : "University Freshman", "school" : "Harvard University", "skypeId" : "Karleigh Moore", "dateOfBirth" : "1993-12-19T00:00:00Z", "nickname" : "", "middleName" : "", "alternativeEmail" : "karleighmoore19@gmail.com", "archived" : false, "onHold" : false, "inPool" : false, "requestToken" : "8iEdNNABYi6NhDZfiPsdDKGSvvujN4Lo", "rejected" : false, "approved" : true, "verificationToken" : "sEw6IlUVt4lUu2leyEVWftUtb8NIaOCH", "verified" : true, "__v" : 0 };
+var TEST_USER = { "_id" : "588b9d45b3aada3ec847430c", "username" : "user1", "password" : "$2a$10$rNXfY2VdUZ2eCRVVOwjS2.4uagb1rbTaQRUMrQH3/uO5CIs9KDv9q", "role" : "Tutor", "email" : "email1+osiusj@gmail.com", "firstName" : "Jane", "lastName" : "Doe", "phoneNumber" : "5458675309", "gender" : "Female", "interests" : [ "Business", "International Relations" ], "nationality" : "United States", "timezone" : "America/Whitehorse", "region" : "Guam", "country" : "United States", "major" : "Management", "enrolled" : "false", "educationLevel" : "University Freshman", "school" : "Harvard University", "skypeId" : "Karleigh Moore", "dateOfBirth" : "1993-12-19T00:00:00Z", "nickname" : "", "middleName" : "", "alternativeEmail" : "email19@gmail.com", "archived" : false, "onHold" : false, "inPool" : false, "requestToken" : "8iEdNNABYi6NhDZfiPsdDKGSvvujN4LL", "rejected" : false, "approved" : true, "verificationToken" : "sEw6IlUVt4lUu2leyEVWftUtb8NIaOLOL", "verified" : true, "__v" : 0 };
 var earliestTime = "2017-01-01";
+
+var TEST_USER_2 = { "_id" : "699b9d45b3aada3ec847430e", "username" : "user2", "password" : "$2a$10$rNXfY2VdUZ2eCRVVOwjS2.4uagb1rbTaQRUMrQH3/uO5CIs9KDlol", "role" : "Student", "email" : "email2+osiusj@gmail.com", "firstName" : "John", "lastName" : "Smith", "phoneNumber" : "6166166167", "gender" : "Male", "interests" : [ "Business" ], "nationality" : "Canada", "timezone" : "America/Whitehorse", "region" : "Michigan", "country" : "United States", "major" : "Management", "enrolled" : "false", "educationLevel" : "University Freshman", "school" : "Harvard University", "skypeId" : "Karleigh Moore", "dateOfBirth" : "1993-12-19T00:00:00Z", "nickname" : "", "middleName" : "", "alternativeEmail" : "karleighmoore19@gmail.com", "archived" : false, "onHold" : false, "inPool" : false, "requestToken" : "8iEdNNABYi6NhDZfiPsdDKGSvvujN4Lo", "rejected" : false, "approved" : true, "verificationToken" : "sEw6IlUVt4lUu2leyEVWftUtb8NIaOCH", "verified" : true, "__v" : 0 };
 
 
 describe('Registration', function(){
@@ -71,6 +73,81 @@ describe('Registration', function(){
                 Registration.createRegistration(TEST_USER, "MALE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
                     assert(err);
                     done();
+                });
+            }); 
+        });
+    });
+
+    describe('getUnmatchedRegistrationsForUser', function(){
+        it('should get one unmatched registration', function (done){
+            Registration.createRegistration(TEST_USER, "NONE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                assert.equal(err, null);
+                Registration.getUnmatchedRegistrationsForUser(TEST_USER, function(err, registration){
+                    assert.equal(registration.length, 1);
+                    done();
+                });
+            }); 
+        });
+    });
+
+
+    describe('findRegistration', function(){
+        it('should find the one unmatched registration we just made by its id', function (done){
+            Registration.createRegistration(TEST_USER, "NONE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                assert.equal(err, null);
+                var reg1 = registration;
+                var regId1 = registration._id;
+                Registration.findRegistration(regId1, TEST_USER, function(err, registration2){
+                    assert.equal(regId1.toString(), registration2._id.toString());
+                    done();
+                });
+            }); 
+        });
+    });
+
+    describe('findRegistration', function(){
+        it('should return an error because it looks up a registration made by another user', function (done){
+            Registration.createRegistration(TEST_USER, "NONE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                assert.equal(err, null);
+                var reg1 = registration;
+                var regId1 = registration._id;
+                Registration.findRegistration(regId1, TEST_USER_2, function(err, registration2){
+                    assert(err);
+                    done();
+                });
+            }); 
+        });
+    });
+
+    describe('deleteRegistration', function(){
+        it('should sucessfully delete the registration since the registration belongs to the user (will raise an error because we search for a deleted reg)', function (done){
+            Registration.createRegistration(TEST_USER, "NONE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                assert.equal(err, null);
+                var reg1 = registration;
+                var regId1 = registration._id;
+                Registration.deleteRegistration(regId1, TEST_USER, function(err, registration2){
+                    Registration.findRegistration(regId1, TEST_USER, function(err, registration3){
+                        assert(err);
+                        done();
+                    });
+                    
+                });
+            }); 
+        });
+    });
+
+    describe('deleteRegistration', function(){
+        it('should fail to delete the registration since the registration belongs to another user', function (done){
+            Registration.createRegistration(TEST_USER, "NONE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                assert.equal(err, null);
+                var reg1 = registration;
+                var regId1 = registration._id;
+                Registration.deleteRegistration(regId1, TEST_USER_2, function(err, registration2){
+                    Registration.findRegistration(regId1, TEST_USER, function(err, registration3){
+                        assert.equal(regId1.toString(), registration3._id.toString());
+                        done();
+                    });
+                    
                 });
             }); 
         });
