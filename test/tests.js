ac79bbcdac9bcfd93c299468227487801c512cf9
Karleigh Moore <karleighmoore19@gmail.com>
2017-01-27 21:04:33 -0500

    Added tests

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,10 @@
 var assert = require('assert');
 
+var expect = require('chai').expect;
 var mongoose = require("mongoose");
 var Registration = require('../models/registration.js');
 var User = require('../models/user.js');
 var db = mongoose.connect('mongodb://localhost/testdb');
-
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
 
 
 var availTest = { '0':[[ '23:00', '24:00' ] ], '1': [],'2': [],'3': [],'4': [],'5': [],'6': [[ '06:00', '08:30' ]] }; 
@@ -21,30 +13,72 @@ var earliestTime = "2017-01-01";
 
 
 describe('Registration', function(){
-        after(function () {
-            // disconnect for clean up
-            db.disconnect();
-        });
+    after(function () {
+        // disconnect for clean up
+        db.disconnect();
+    });
 
-        // drop database before each test
-        beforeEach(function (done) {
-            mongoose.connection.db.dropDatabase(done);
-        });
+    // drop database before each test
+    beforeEach(function (done) {
+        mongoose.connection.db.dropDatabase(done);
+    });
 
-        describe('createRegistration', function(){
-            it('should do the create a registration object with the specified fields (in this test, all required fields are filled out)', function (done){
-                    Registration.createRegistration(TEST_USER, "MALE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
-                        if (err){console.log(err);}
-                        
-                        else{
-                        assert.equal(registration.availability, availTest);
-                        done();}
-                    }
+
+    describe('createRegistration', function(){
+        it('should do the create a registration object with the specified fields (in this test, all required fields are filled out)', function (done){
+            Registration.createRegistration(TEST_USER, "MALE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                if (err){console.log(err);}
+                
+                else{
+                    assert.equal(registration.availability, availTest);
+                    assert.equal(registration.genderPref, "MALE");
+                    assert.equal(registration.courses, "COURSES_THESE");
+                    assert.equal(registration.isMatched, false);
+                    done();}
+                }
             );
         });
     });
-});
 
+    describe('createRegistration', function(){
+        it('should do the create a registration object with the specified fields (in this test, all required fields are filled out)', function (done){
+            Registration.createRegistration(TEST_USER, "MALE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                
+                assert.equal(registration.availability, availTest);
+                assert.equal(registration.genderPref, "MALE");
+                assert.equal(registration.courses, "COURSES_THESE");
+                assert.equal(registration.isMatched, false);
+                done();}
+                
+            );
+        });
+    });
+
+    describe('createRegistration', function(){
+        it('should fail to create registration object since all fields are not filled out in a valid way', function (done){
+            Registration.createRegistration(TEST_USER, "INVALID OPTION", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                assert(err);
+                done();
+
+            }); 
+        });
+    });
+
+    describe('createRegistration', function(){
+        it('should fail to create a second registration after making one sucessfully', function (done){
+            Registration.createRegistration(TEST_USER, "NONE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                assert.equal(err, null);
+                Registration.createRegistration(TEST_USER, "MALE", availTest, "COURSES_THESE", earliestTime, function(err, registration){
+                    assert(err);
+                    done();
+                });
+            }); 
+        });
+    });
+
+
+
+});
 
 
 
